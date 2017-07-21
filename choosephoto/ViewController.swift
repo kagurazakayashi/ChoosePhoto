46eea0f5b3d85c607d6beb8d7a528ed614358a2a
神楽坂雅詩 <cyscxchope@gmail.com>
2017-07-21 22:35:13 +0800

    创建空白工程

---
+++ choosephoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  choosephoto
+//
+//  Created by 神楽坂雅詩 on 2017/7/21.
+//  Copyright © 2017年 KagurazakaYashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
