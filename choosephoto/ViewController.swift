b4fcf0b17316f675a3a74ff6bb1f4ebcc380aa58
神楽坂雅詩 <cyscxchope@gmail.com>
2017-07-22 15:48:07 +0800

    摄像头捕捉并预览

--- choosephoto/ViewController.swift
+++ choosephoto/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     var 视频捕获会话: AVCaptureSession!
     var 视频捕获输入: AVCaptureDeviceInput!
     var 视频捕获输出: AVCaptureVideoDataOutput!
+    var 视频捕获启动:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,6 +82,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             print("没能启动视频捕获设备")
             return false
         }
+        视频捕获输出 = AVCaptureVideoDataOutput()
         do{
             try 视频捕获输入 = AVCaptureDeviceInput(device: 视频捕获设备!)
         } catch let error as NSError {
@@ -98,7 +100,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let 视频像素宽度V = NSNumber(value: 1280)
         let 视频像素高度K = kCVPixelBufferHeightKey as String
         let 视频像素高度V = NSNumber(value: 720)
-        //TODO: fatal error: unexpectedly found nil while unwrapping an Optional value.
         视频捕获输出.videoSettings = [视频像素模式K:视频像素模式V, 视频像素宽度K:视频像素宽度V, 视频像素高度K:视频像素高度V]
         if(视频捕获会话.canAddInput(视频捕获输入)){
             视频捕获会话.addInput(视频捕获输入)
@@ -112,21 +113,53 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             print("视频捕获输出设置失败！")
             return false
         }
-//        let subQueue:dispatch_queue_t = dispatch_queue_create("subQueue", nil)
-//        captureOutput.setSampleBufferDelegate(self, queue: subQueue)
+        let 视频捕获线程 = DispatchQueue(label: "subQueue")
+        视频捕获输出.setSampleBufferDelegate(self, queue: 视频捕获线程)
+        
         视频捕获预览 = AVCaptureVideoPreviewLayer(session: 视频捕获会话)
+        视频捕获预览.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
         视频捕获预览.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        视频捕获预览.frame = 实时预览框.layer.frame
-        实时预览框.layer.addSublayer(视频捕获预览)
+        self.view.layer.addSublayer(视频捕获预览)
+//        视频捕获预览.isHidden = true
+        
         视频捕获会话.commitConfiguration()
+        视频捕获启动 = true
         return true
     }
-
+    
+    func 从数据流创建图片(缓冲区:CMSampleBuffer!) -> UIImage {
+        let 图片缓冲区:CVImageBuffer = CMSampleBufferGetImageBuffer(缓冲区)!
+        CVPixelBufferLockBaseAddress(图片缓冲区, CVPixelBufferLockFlags(rawValue: 0))
+        let 逐行大小:size_t = CVPixelBufferGetBytesPerRow(图片缓冲区)
+        let 宽度:size_t = CVPixelBufferGetWidth(图片缓冲区)
+        let 高度:size_t = CVPixelBufferGetHeight(图片缓冲区)
+        let 安全点:UnsafeMutableRawPointer = CVPixelBufferGetBaseAddress(图片缓冲区)!
+        let 位图信息:UInt32 = CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue
+        let 色彩空间: CGColorSpace = CGColorSpaceCreateDeviceRGB()
+        let 画布:CGContext = CGContext(data: 安全点, width: 宽度, height: 高度, bitsPerComponent: 8, bytesPerRow: 逐行大小, space: 色彩空间, bitmapInfo: 位图信息)!
+        let 取出图片: CGImage = 画布.makeImage()!
+        CVPixelBufferUnlockBaseAddress(图片缓冲区, CVPixelBufferLockFlags(rawValue: 0))
+        return UIImage(cgImage: 取出图片, scale: 1, orientation: UIImageOrientation.right)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let 当前图片 = 从数据流创建图片(缓冲区: sampleBuffer)
+        DispatchQueue.main.async() { () -> Void in
+            self.实时预览框.image = 当前图片
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        视频捕获会话.startRunning()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        视频捕获会话.stopRunning()
+        super.viewWillDisappear(animated)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print("内存不足！")
     }
-
-
 }
 
